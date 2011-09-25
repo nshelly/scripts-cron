8a4cdee1057d06da15eca049eeda8b9d18c9eea1
Nicholas Shelly <nico.shelly@gmail.com>
2011-09-25 14:25:52 -0400

    Added views.py template

---
+++ views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import get_object_or_404, render_to_response
+from django.http import HttpResponseRedirect, HttpResponse
+from django.core.urlresolvers import reverse
+
+def manage(request):
+    pass
+
+def add(request):
+    pass
+
+def edit(request):
+    pass
+
