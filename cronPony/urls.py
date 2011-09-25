fa4d8677e50fc2f67209de6d1db8116fbea371a6
Steven Allen <steven@stebalien.com>
2011-09-25 14:53:12 -0400

    Don't use job ids in the add view

--- cronPony/urls.py
+++ cronPony/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns('',
     url(r'^$', 'views.manage', name="manage"),
-    url(r'^add/(?P<path>.*)$', 'views.add', name="add"),
-    url(r'^edit/(?P<path>.*)$', 'views.add', name="edit"),
+    url(r'^add/$', 'views.add', name="add"),
+    url(r'^edit/(?P<job>.*)$', 'views.add', name="edit"),
 )
