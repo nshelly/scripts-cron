6f486fcc25d1288890bd3df7f71215591fa46a12
Nicholas Shelly <nico.shelly@gmail.com>
2011-09-25 15:55:55 -0400

    Added basic cronjob form

--- views.py
+++ views.py
@@ -1,12 +1,22 @@
 from django.shortcuts import get_object_or_404, render_to_response
 from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
+from cronPony.forms import CronjobForm
 
 def manage(request):
     pass
 
 def add(request):
-    pass
+    if request.method == 'GET':
+        form = CronjobForm(request.GET)
+        if form.is_valid():
+            return HttpResponseReirect(manage)
+    else:
+        form = CronjobForm()
+
+    return render_to_response('cronPony/add.html', {
+        'form' : form,
+    })
 
 def edit(request):
     pass
